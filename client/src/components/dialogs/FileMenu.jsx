b957a8f04e4d41569ce1df6de76680e722ad1add
Subin Kumar <isubinkr@icloud.com>
2024-08-21 12:16:28 +0530

    add chat & file menu component

---
+++ client/src/components/dialogs/FileMenu.jsx
@@ -0,0 +1,20 @@
+import { Menu } from "@mui/material";
+
+const FileMenu = ({ anchorEl }) => {
+  return (
+    <Menu anchorEl={anchorEl}>
+      <div
+        style={{
+          width: "10rem",
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam,
+        ex ipsa repellendus accusamus adipisci, facere amet repudiandae ab aut
+        possimus reprehenderit accusantium culpa nobis quo quasi, animi ipsum
+        officiis!
+      </div>
+    </Menu>
+  );
+};
+
+export default FileMenu;
