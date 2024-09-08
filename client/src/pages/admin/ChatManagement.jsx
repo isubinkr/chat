cfe18e99bcbde078cc03bc74c718eedbdabf0011
Subin Kumar <isubinkr@icloud.com>
2024-09-08 11:53:54 +0530

    add admin dashboard component

---
+++ client/src/pages/admin/ChatManagement.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../../components/layout/AdminLayout";
+
+const ChatManagement = () => {
+  return (
+    <AdminLayout>
+      <div>ChatMangement</div>
+    </AdminLayout>
+  );
+};
+
+export default ChatManagement;
