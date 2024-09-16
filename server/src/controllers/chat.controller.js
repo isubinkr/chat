bf8d04689254adfa0cbecce82b1f012400652e2a
Subin Kumar <isubinkr@icloud.com>
2024-09-17 04:00:34 +0530

    add allucgroups & add-members endpoint

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -1,6 +1,7 @@
 import { ALERT, REFETCH_CHATS } from "../constants/events.js";
 import { getOtherMember } from "../lib/helper.js";
 import { Chat } from "../models/chat.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiRespose.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -62,4 +63,75 @@ const getAllChats = asyncHandler(async (req, res) => {
     );
 });
 
-export { createGroupChat, getAllChats };
+const getAllUserCreatedGroups = asyncHandler(async (req, res) => {
+  const chats = await Chat.find({
+    members: req.user._id,
+    groupChat: true,
+    creator: req.user._id,
+  }).populate("members", "name avatar");
+
+  const groups = chats.map(({ members, _id, groupChat, name }) => ({
+    _id,
+    groupChat,
+    name,
+    avatar: members.slice(0, 3).map(({ avatar }) => avatar.url),
+  }));
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        groups,
+        "All user created groups fetched successfully"
+      )
+    );
+});
+
+const addMembers = asyncHandler(async (req, res) => {
+  const { chatId, members } = req.body;
+
+  if (!members || members.length < 1)
+    throw new ApiError(400, "Members not found");
+
+  const chat = await Chat.findById(chatId);
+
+  if (!chat) throw new ApiError(404, "Chat not found");
+
+  if (!chat.groupChat) throw new ApiError(400, "Not a group chat");
+
+  if (chat.creator.toString() !== req.user._id.toString())
+    throw new ApiError(403, "Not allowed to add member");
+
+  const allNewMembersPromise = members.map((i) => User.findById(i, "name"));
+
+  const allNewMembers = await Promise.all(allNewMembersPromise);
+
+  const uniqueMembers = allNewMembers
+    .filter((i) => !chat.members.includes(i._id.toString()))
+    .map((i) => i._id);
+
+  chat.members.push(...uniqueMembers);
+
+  if (chat.members.length > 100)
+    throw new ApiError(400, "Group members limit reached");
+
+  await chat.save();
+
+  const allUsersName = allNewMembers.map((i) => i.name).join(", ");
+
+  emitEvent(
+    req,
+    ALERT,
+    chat.members,
+    `${allUsersName} has been added in the group`
+  );
+
+  emitEvent(req, REFETCH_CHATS, chat.members);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Members added successfully"));
+});
+
+export { createGroupChat, getAllChats, getAllUserCreatedGroups, addMembers };
--- server/src/routes/chat.routes.js
+++ server/src/routes/chat.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
+  addMembers,
   createGroupChat,
   getAllChats,
+  getAllUserCreatedGroups,
 } from "../controllers/chat.controller.js";
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 // secured routes
 router.route("/create-gc").post(verifyJWT, createGroupChat);
 router.route("/allchats").get(verifyJWT, getAllChats);
+router.route("/allucgroups").get(verifyJWT, getAllUserCreatedGroups);
+router.route("/add-members").put(verifyJWT, addMembers);
 
 export default router;
